f2738a32dfd6e28ff88a7fc4c3082021feeb6846
Anna Lukianova <ann.lookyanova@gmail.com>
2020-05-19 21:49:36 +0200

    Make speed-button's functions

--- index.js
+++ index.js
@@ -8,28 +8,67 @@ canvas.width = 400;
 const columns = canvas.width / resolution;
 const rows = canvas.height / resolution;
 
+let grid = buildGrid();
+let nextTime = Date.now()
+
+const speedButtonTwo = document.getElementById("speed-two")
+const speedButtonFour = document.getElementById("speed-four")
+const clearButton = document.getElementById("clear")
 
 function buildGrid() {
     return new Array(columns).fill(null)
         .map(() => new Array(rows).fill(null)
             .map(() => Math.floor(Math.random() * 2)));
 }
-let grid = buildGrid();
 
-let nextTime = Date.now()
-
+function render(grid) {
+    for (let col = 0; col < grid.length; col++) {
+        for (let row = 0; row < grid[col].length; row++) {
+            const cell = grid[col][row];
+            ctx.beginPath();
+            ctx.rect(col * resolution, row * resolution, resolution, resolution);
+            ctx.fillStyle = cell ? 'grey' : 'darkgrey';
+            ctx.fill();
+        }
+    }
+}
 requestAnimationFrame(update)
+
 function update() {
     const now = Date.now()
     if (now >= nextTime) {
         grid = nextGeneration(grid)
         render(grid);
-        if (now > nextTime + 100) {
+        if (now > nextTime + 1000) {
             nextTime = now
         }
-        nextTime += 100
+        nextTime += 1000
     }
-    requestAnimationFrame(update)
+ requestAnimationFrame(update)
+}
+
+button()
+function button() {
+    speedButtonTwo.addEventListener('click', speedTwo)
+    speedButtonFour.addEventListener('click', speedFour)
+    clearButton.addEventListener('click', clear)
+}
+
+function speedTwo() {
+nextTime-=250
+requestAnimationFrame(speedTwo)
+}
+
+
+function speedFour() {
+    nextTime-=500
+    requestAnimationFrame(speedFour)
+}
+
+
+function clear() {
+   console.log(nextGeneration)
+ 
 }
 
 function nextGeneration() {
@@ -50,9 +89,7 @@ function nextGeneration() {
                     if (x_cell >= 0 && y_cell >= 0 && x_cell < columns && y_cell < rows) {
                         const currentNeighbor = grid[col + i][row + j]
                         numNeighbors += currentNeighbor;
-
                     }
-
                 }
             }
             //rules
@@ -67,18 +104,5 @@ function nextGeneration() {
     }
     return nextGeneration
 }
+
 nextGeneration()
-function render(grid) {
-    for (let col = 0; col < grid.length; col++) {
-        for (let row = 0; row < grid[col].length; row++) {
-            const cell = grid[col][row];
-
-            ctx.beginPath();
-            ctx.rect(col * resolution, row * resolution, resolution, resolution);
-            ctx.fillStyle = cell ? 'green' : 'white';
-            ctx.fill();
-            ctx.stroke();
-        }
-    }
-
-}
