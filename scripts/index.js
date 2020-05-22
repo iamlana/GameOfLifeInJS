1edd4611288f1146bd5fbebc54fd0573362fd907
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-05-22 18:47:30 +0200

    Add play / pause and fix time

--- scripts/index.js
+++ scripts/index.js
@@ -5,35 +5,48 @@ import { buildGrid, clearGrid, randomizeGrid } from './grid.js'
 const ctx = setupCanvas();
 
 let grid = buildGrid();
-let nextTime = Date.now()
+let nextTime = Date.now();
+let updateInterval = 500;
+let running = false;
 
 requestAnimationFrame(onAnimationFrame)
 function onAnimationFrame() {
-  const now = Date.now()
-  if (now >= nextTime) {
+  const now = Date.now();
+  if (running && now >= nextTime) {
     update(grid);
-    render(ctx, grid);
-    if (now > nextTime + 1000) {
+    if (now > nextTime + updateInterval) {
       nextTime = now
     }
-    nextTime += 1000
+    nextTime += updateInterval;
   }
+  render(ctx, grid);
   requestAnimationFrame(onAnimationFrame)
 }
 
-document.getElementById("speed-two").addEventListener('click', speedTwo)
-document.getElementById("speed-four").addEventListener('click', speedFour)
-document.getElementById("clear").addEventListener('click', clear)
-document.getElementById("random").addEventListener('click', randomize)
+document.getElementById('play').addEventListener('click', play)
+document.getElementById('pause').addEventListener('click', pause)
+document.getElementById('speed-two').addEventListener('click', speedTwo)
+document.getElementById('speed-four').addEventListener('click', speedFour)
+document.getElementById('clear').addEventListener('click', clear)
+document.getElementById('random').addEventListener('click', randomize)
+
+function play () {
+  updateInterval = 500;
+  running = true;
+}
+
+function pause () {
+  running = false;
+}
 
 function speedTwo() {
-  nextTime -= 250
-  requestAnimationFrame(speedTwo)
+  updateInterval = 250;
+  running = true;
 }
 
 function speedFour() {
-  nextTime -= 500
-  requestAnimationFrame(speedFour)
+  updateInterval = 125;
+  running = true;
 }
 
 function clear() {
