861ceb48c4af0cb05aa83bbba5c50ee8e0fb40db
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-05 14:42:08 +0200

    Refactor drawing

--- scripts/setupMouseListeners.js
+++ scripts/setupMouseListeners.js
@@ -1,14 +1,26 @@
 import { PIXELS_PER_CELL } from './constants.js';
 
-export function setupMouseListeners(canvas, grid, reRender) {
-  canvas.onmousedown = function (event) {
-    canvas.onmousemove = function (event) {
-      var x = Math.floor(event.offsetX/PIXELS_PER_CELL);
-      var y = Math.floor(event.offsetY/PIXELS_PER_CELL);
-      grid.set(x, y, 1);
-    };
-    canvas.onmouseup = function () {
-      canvas.onmousemove = null;
-    };
-  };
+export function setupMouseListeners(canvas, grid) {
+  let drawing = false
+
+  canvas.addEventListener('mousedown', function (event) {
+    drawing = true
+    draw(event)
+  })
+
+  canvas.addEventListener('mousemove', function (event) {
+    if (drawing) {
+      draw(event)
+    }
+  })
+
+  canvas.addEventListener('mouseup', function () {
+    drawing = false
+  })
+
+  function draw (event) {
+    var x = Math.floor(event.offsetX / PIXELS_PER_CELL);
+    var y = Math.floor(event.offsetY / PIXELS_PER_CELL);
+    grid.set(x, y, 1);
+  }
 }
