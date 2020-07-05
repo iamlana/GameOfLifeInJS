a58578ca5b01060ed4a8a07014571a799c32c5f6
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-05 14:34:50 +0200

    Drawing on board

--- scripts/setupMouseListeners.js
+++ scripts/setupMouseListeners.js
@@ -3,13 +3,9 @@ import { PIXELS_PER_CELL } from './constants.js';
 export function setupMouseListeners(canvas, grid, reRender) {
   canvas.onmousedown = function (event) {
     canvas.onmousemove = function (event) {
-      // var x = event.offsetX;
-      // var y = event.offsetY;
-      grid.set(1, 3, 1);
-      grid.set(2, 3, 1);
-      grid.set(3, 3, 1);
-      grid.set(3, 2, 1);
-      grid.set(2, 1, 1);
+      var x = Math.floor(event.offsetX/PIXELS_PER_CELL);
+      var y = Math.floor(event.offsetY/PIXELS_PER_CELL);
+      grid.set(x, y, 1);
     };
     canvas.onmouseup = function () {
       canvas.onmousemove = null;
