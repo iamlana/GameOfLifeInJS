6a7867814470e34d4eef134dba7f938897d98ec3
Anna Lukianova <ann.lookyanova@gmail.com>
2020-07-21 16:04:39 +0200

    Fix desctop version mouse event problem

--- scripts/setupMouseListeners.js
+++ scripts/setupMouseListeners.js
@@ -31,8 +31,15 @@ export function setupMouseListeners(canvas, grid) {
   })
 
   function draw(event) {
-    var x = Math.floor(event.offsetX / PIXELS_PER_CELL);
-    var y = Math.floor(event.offsetY / PIXELS_PER_CELL);
-    grid.set(x, y, 1);
+
+    if (event.type == "touchmove") {
+      var x = Math.floor(event.touches[0].clientX / PIXELS_PER_CELL);
+      var y = Math.floor(event.touches[0].clientY / PIXELS_PER_CELL);
+      grid.set(x, y, 1);
+    } else {
+      var x = Math.floor(event.offsetX / PIXELS_PER_CELL);
+      var y = Math.floor(event.offsetY / PIXELS_PER_CELL);
+      grid.set(x, y, 1);
+    }
   }
 }
